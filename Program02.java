4f27f07d979e9adbdfe06b3e7709e9e76fd0007d
Revanth  Chandaka <Revanth.Chandaka@godigit.com>
2024-07-31 14:23:57 +0530

    program2 changes

---
+++ Program02.java
@@ -0,0 +1,20 @@
+public class Program02 {
+    static int a;
+    static long b;
+    static byte c;
+    static short d;
+    static float e;
+    static double f;
+    static char g;
+    static boolean h;
+    public static void main(String[] args) {
+        System.out.println("Int:"+a);
+        System.out.println("Long:"+b);
+        System.out.println("Byte:"+c);
+        System.out.println("Short:"+d);
+        System.out.println("Float:"+e);
+        System.out.println("Double:"+f);
+        System.out.println("Char:"+g);
+        System.out.println("Boolean:"+h);
+    }
+}
